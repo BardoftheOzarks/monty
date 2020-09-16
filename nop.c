f4762b7a0fee0bc48450818ab6f08374bdbca7c6
BardoftheOzarks <1886@holbertonschool.com>
2020-09-16 18:11:41 +0000

    added nop

---
+++ nop.c
@@ -0,0 +1,10 @@
+#include "monty.h"
+/**
+ * nop - absolutely nothing
+ * @stack: really?
+ * @line_number: which line of script we're on
+ */
+void nop(stack_t **stack, unsigned int line_number)
+{
+	return;
+}
