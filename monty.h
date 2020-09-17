a2bb9bbe8669c511004d17bea3885a2794be6ecb
cort-robinson <cortrobinson99@gmail.com>
2020-09-17 21:53:49 +0000

    betty fixes

--- monty.h
+++ monty.h
@@ -18,9 +18,9 @@ int var;
  */
 typedef struct stack_s
 {
-        int n;
-        struct stack_s *prev;
-        struct stack_s *next;
+		int n;
+		struct stack_s *prev;
+		struct stack_s *next;
 } stack_t;
 
 /**
@@ -33,8 +33,8 @@ typedef struct stack_s
  */
 typedef struct instruction_s
 {
-        char *opcode;
-        void (*f)(stack_t **stack, unsigned int line_number);
+		char *opcode;
+		void (*f)(stack_t **stack, unsigned int line_number);
 } instruction_t;
 
 void *get_func(char *str);
@@ -43,6 +43,7 @@ void pop(stack_t **stack, unsigned int line_number);
 void pint(stack_t **stack, unsigned int line_number);
 void pall(stack_t **stack, unsigned int line_number);
 void nop(stack_t **stack, unsigned int line_number);
+void error(int errno, int *line_number, char *str);
 void free_stack(stack_t *stack);
 
 #endif /*MONTY_H*/
