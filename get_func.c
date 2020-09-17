1fe9258344b8dab358506bbc1f4525a4f22bd500
cort-robinson <cortrobinson99@gmail.com>
2020-09-17 01:05:38 +0000

    add lots

--- get_func.c
+++ get_func.c
@@ -4,22 +4,22 @@
  * @str: input command
  * Return: function pointer
  */
-void (*get_func(char *str))(stack_t **stack, unsigned int line_number)
+void *get_func(char *str)
 {
 	instruction_t commands[] = {
 		{"push", push},
 		{"pall", pall},
 		{"pint", pint},
 		{"pop", pop},
-		{"swap", swap},
-		{"add", add},
+		/* {"swap", swap},
+		{"add", add}, */
 		{"nop", nop},
 		{NULL, NULL}
 	};
 	int i;
 
-	for (i = 0; commands[i].opcode; i++)
-		if (commands[i].opcode == *str)
-			return (commands[i].f);
+	for (i = 0; (&commands[i])->opcode; i++)
+		if (strncmp((&commands[i])->opcode, str, 3) == 0)
+			return ((&commands[i])->f);
 	return (NULL);
 }
--- main.c
+++ main.c
@@ -8,24 +8,63 @@
  */
 int main(int ac, char **av)
 {
-	/*Declare struct *head, int line_number???*/
+	FILE *fptr;
+	stack_t *stack;
+	void (*function)(stack_t **stack, int line_number);
+	char buffer[128];
+	int i = 2;
+	char *token = NULL, *token2;
+	int line_number = 0;
+
+
+	if (ac != 2)
+	{
+		fprintf(stderr, "USAGE: monty file\n");
+		exit(EXIT_FAILURE);
+	}
 
 	/*open file from av[1]
 	  Error: usage*/
-
+	fptr = fopen(av[1], "r");
+	if (fptr == NULL)
+	{
+		fprintf(stderr, "Can't open file %s\n", av[1]);
+		exit(EXIT_FAILURE);
+	}
 
 	/*loop to read by line, one command/line
 	  Note: spacing, update line_number*/
+	while (fgets(buffer, sizeof(buffer), fptr))
+	{
+		line_number++;
+		/*feed first word to get_func
+		Error: unknown instruction: func == NULL*/
+		while (token == NULL)
+			token = strtok(buffer, " ");
+		if (token == "push")
+		{
+			while (token2 == NULL)
+				token2 = strtok(NULL, " ");
+			if (token2 != NULL)
+				var = atoi(token2);
+			else
+			{
+				fprintf(stderr, "L%d: usage: push integer\n", line_number);
+				exit(EXIT_FAILURE);
+			}
+		}
+		function = get_func(token);
+		if (function == NULL)
+		{
+			fprintf(stderr, "L%d: unknown instruction %s\n", line_number, token);
+			exit(EXIT_FAILURE);
+		}
 
-	/*feed first word to get_func
-	  Error: unknown instruction: func == NULL*/
+		(*function)(&stack, line_number);
+	}
 
-	/*perform function on stack
-	  Note: figure out how to pass argument for push
-	  global variable?*/
+	fclose(fptr);
+	free_stack(stack);
 
-	/*end loop*/
-
-
-	/*close, free, and EXIT_SUCCESS*/
+	exit(EXIT_SUCCESS);
 }
